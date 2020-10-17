d3d37c1d867da69e7319544dbfb481838c560320
3bHussein <39864308+3bHussein@users.noreply.github.com>
2020-10-17 20:56:21 +0200

    Full Fetch

---
+++ petroleum-website/petroleum-website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="petroleum_website.MvcApplication" Language="C#" %>
---
+++ petroleum-website/petroleum-website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(petroleum_website.Startup))]
+namespace petroleum_website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
